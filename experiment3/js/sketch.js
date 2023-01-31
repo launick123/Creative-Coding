0f823baf3484b9a83b5a1202998de0ae3df1fe46
Nicholas Lau <nklau@ucsc.edu>
2023-01-30 23:52:59 -0800

    experiment 3

--- experiment3/js/sketch.js
+++ experiment3/js/sketch.js
@@ -1,67 +1,93 @@
-// sketch.js - purpose and description here
-// Author: Your Name
-// Date:
+// P_2_2_4_01
+//
+// Generative Gestaltung – Creative Coding im Web
+// ISBN: 978-3-87439-902-9, First Edition, Hermann Schmidt, Mainz, 2018
+// Benedikt Groß, Hartmut Bohnacker, Julia Laub, Claudius Lazzeroni
+// with contributions by Joey Lee and Niels Poldervaart
+// Copyright 2018
+//
+// http://www.generative-gestaltung.de
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-// Here is how you might set up an OOP p5.js project
-// Note that p5.js looks for a file called sketch.js
+/**
+ * limited diffusion aggregation
+ *
+ * KEYS
+ * s                   : save png
+ */
+'use strict';
 
-// Constants - User-servicable parts
-// In a longer project I like to put these in a separate file
-const VALUE1 = 1;
-const VALUE2 = 2;
+var maxCount = 5000; // max count of the cirlces
+var currentCount = 1;
+var x = [];
+var y = [];
+var r = [];
+let red,gre,blu;
 
-// Globals
-let myInstance;
-let canvasContainer;
-
-class MyClass {
-    constructor(param1, param2) {
-        this.property1 = param1;
-        this.property2 = param2;
-    }
-
-    myMethod() {
-        // code to run when method is called
-    }
-}
-
-// setup() function is called once when the program starts
 function setup() {
-    // place our canvas, making it fit our container
-    canvasContainer = $("#canvas-container");
-    let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
-    canvas.parent("canvas-container");
-    // resize canvas is the page is resized
-    $(window).resize(function() {
-        console.log("Resizing...");
-        resizeCanvas(canvasContainer.width(), canvasContainer.height());
-    });
-    // create an instance of the class
-    myInstance = new MyClass(VALUE1, VALUE2);
+  createCanvas(800, 800);
+  strokeWeight(0.5);
+  colorMode(HSB, 255, 255, 255)
+  red = random(255);
+  gre = random(255);
+  blu = random(255);
+  
 
-    var centerHorz = windowWidth / 2;
-    var centerVert = windowHeight / 2;
+  // first circle
+  x[0] = width / 2;
+  y[0] = height / 2;
+  r[0] = 10;
 }
 
-// draw() function is called repeatedly, it's the main animation loop
 function draw() {
-    background(220);    
-    // call a method on the instance
-    myInstance.myMethod();
+  clear();
 
-    // Put drawings here
-    var centerHorz = canvasContainer.width() / 2 - 125;
-    var centerVert = canvasContainer.height() / 2 - 125;
-    fill(234, 31, 81);
-    noStroke();
-    rect(centerHorz, centerVert, 250, 250);
-    fill(255);
-    textStyle(BOLD);
-    textSize(140);
-    text("p5*", centerHorz + 10, centerVert + 200);
+  // create a random set of parameters
+  var newR = random(1, 7);
+  var newX = random(newR, width - newR);
+  var newY = random(newR, height - newR);
+
+  var closestDist = Number.MAX_VALUE;
+  var closestIndex = 0;
+  // which circle is the closest?
+  for (var i = 0; i < currentCount; i++) {
+    var newDist = dist(newX, newY, x[i], y[i]);
+    if (newDist < closestDist) {
+      closestDist = newDist;
+      closestIndex = i;
+    }
+  }
+
+  // show original position of the circle and a line to the new position
+   fill(230);
+   ellipse(newX, newY, newR * 2, newR * 2);
+   line(newX, newY, x[closestIndex], y[closestIndex]);
+
+  // aline it to the closest circle outline
+  var angle = atan2(newY - y[closestIndex], newX - x[closestIndex]);
+
+  x[currentCount] = x[closestIndex] + cos(angle) * (r[closestIndex] + newR);
+  y[currentCount] = y[closestIndex] + tan(angle) * (r[closestIndex] + newR);
+  r[currentCount] = newR;
+  currentCount++;
+
+  // draw them
+  for (var i = 0; i < currentCount; i++) {
+    fill(red, gre, blu, 255);
+    rect(x[i], y[i], r[i] * 2, r[i] * 2);
+  }
+
+  if (currentCount >= maxCount) noLoop();
 }
 
-// mousePressed() function is called once after every time a mouse button is pressed
-function mousePressed() {
-    // code to run when mouse is pressed
+function keyReleased() {
+  if (key == 's' || key == 'S') saveCanvas(gd.timestamp(), 'png');
 }

442a05c2333bc8f87583f73d8626043b79f10b20
Nicholas Lau <nklau@ucsc.edu>
2023-03-02 22:52:14 -0800

    exp7

---
+++ experiment7/sketch.js
@@ -0,0 +1,34 @@
+let data , distance, planet;
+let sent = 0;
+
+function preload() {
+  data = loadTable('./data.csv', 'csv', 'header');
+}
+
+function setup() {
+  createCanvas(400, windowHeight);
+  distance = data.getColumn("Distance (M mi)");
+  planet = data.getColumn("Planets");
+}
+
+function draw() {
+  background(0);
+  for (let i = 0; i < distance.length; i++) {
+    rect(i * 40, height - 1, 20, -distance[i] / sent * 8);
+    fill(0, 100, 220);
+    push();
+
+    translate(i * 40 + 10, height - 1 - distance[i] / sent * 8);
+    rotate(radians(-45));
+    fill(0, 200, 220);
+    text(planet[i], 0, 0);
+
+    translate(10,10);
+    text(planet[i],0,0);
+    pop();
+
+  }
+  if (sent < 40) {
+    sent = sent + 1;
+  }
+}
